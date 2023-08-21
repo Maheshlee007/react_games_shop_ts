fdbfd007419eda4fc87e497576cea907130f8de6
Mahesh Lee <maheshlee1234@gmail.com>
2023-08-21 07:23:13 +0530

    changes to form applied

--- src/components/forms/ReactHookForms.tsx
+++ src/components/forms/ReactHookForms.tsx
@@ -1,17 +1,32 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+//zod schema declaration
+const schema = z.object({ email: z.string().email() });
+type FormData = z.infer<typeof schema>;
+
+//component
 const ReactHookForms = () => {
-  const { register, handleSubmit } = useForm();
+  //react hook form destrcuturing
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  console.log(errors);
+
   const postData = (data: FieldValues) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(postData)}>
-      <strong>ReactHookForms</strong>
+      <strong>ReactHookForms with zod schema</strong>
       <div className="form-group m-2 w-75">
         <label htmlFor="name" className="form-label">
           email
         </label>
         <input
-          {...register("email")}
+          // {...register("email", { required: true, minLength: 10 })}
           id="name"
           type="email"
           className="form-control"
@@ -20,6 +35,10 @@ const ReactHookForms = () => {
           send email
         </button>
       </div>
+      {/* {errors.email?.type === "required" && (
+        <p className="text-danger">* mail is required</p>
+      )} */}
+      {errors.email && <p className="text-danger">* {errors.email.message}</p>}
     </form>
   );
 };
